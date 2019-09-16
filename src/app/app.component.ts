a888ad4fd2164a59bbf61150c314f1e7d89a42a0
Eddie Franco <eddiefranco626@gmail.com>
2019-09-15 20:05:24 -0500

    angular crash course from traversey media

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { getLocaleFirstDayOfWeek } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crash-todolist';
+
 }
+
+// showcasing how to use constructors and how they run first. Also shows the :void which means the function returns nothing
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title:string = 'Eddie ToDo List';
+
+//   // constructors run on instansiation
+//   constructor() {
+//     this.changeTitle('John')
+//   }
+
+//   // void means it doesn't return anything
+//   changeTitle(title:string):void {
+//     this.title = title
+//   }
+// }
